124da8c1c6bc93d57a0bbacddcb4ca12ebf3d108
tmquan180302 <tmquan183@gmail.com>
2024-03-12 01:50:02 +0700

    Fix route

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,12 +4,10 @@ const router = express.Router();
 const userController = require('../app/controllers/UserController');
 const { authenToken } = require('../middlewares/auth');
 
-router.post('/login', authenToken, userController.login);
-router.post('/loginGoogle',authenToken, userController.loginGoogle);
-router.post('/register',authenToken, userController.create);
+router.post('/login', userController.login);
+router.post('/loginGoogle', userController.loginGoogle);
+router.post('/register', userController.create);
 router.post('/updatePass',authenToken, userController.updatePass);
-
-
 
 
 
