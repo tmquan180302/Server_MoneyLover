b699248487583475c4276826b84f863fdc7bdbc2
tmquan180302 <tmquan183@gmail.com>
2024-03-16 14:42:28 +0700

    Fix route

--- src/routes/api/budget.js
+++ src/routes/api/budget.js
@@ -12,13 +12,13 @@ const { authenToken } = require('../../middlewares/auth');
 router.get('/allTimeReport', authenToken, budgetController.getAllTimeReport);
 
 // Lấy dữ liệu các bản ghi theo danh mục (Phần onlick của của báo cáo)
-router.get('/:id/report', budgetController.getCateroryReport);
+router.get('/:id/report',authenToken, budgetController.getCateroryReport);
 
 // Gửi lên req là key
-router.post('/find', budgetController.searchAll);
+router.post('/find',authenToken, budgetController.searchAll);
 
 // Báo cáo danh mục toàn kỳ
-router.get('/:type', budgetController.getAllReport);
+router.get('/:type',authenToken, budgetController.getAllReport);
 
 // Lấy dữ liệu báo cáo trong năm 
 // Chi tiêu params type = 0 
